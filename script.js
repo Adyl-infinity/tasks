4c43c876312691b734dc244d4fbe524d57f8fff8
Adyl <inamjanov.a@gmail.com>
2021-07-06 20:08:30 +0600

    master

---
+++ script.js
@@ -0,0 +1,62 @@
+//TASK1
+// let a = 13
+// if (a === 10){
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+
+//TASK 2
+// let a = 300
+// let b = 100
+// if (a < b){
+//     console.log("a меньше b")
+// } else if ( a > b ) {
+//     console.log("a больше b")
+// }
+
+//TASK3
+// let a = 0
+// if (a > 0){
+//     console.log('positive')
+// } else if (a == 0){
+//     console.log('равно')
+// } else if (a < 0){
+//     console.log('negative')
+// }
+
+//TASK4
+// let number = 45
+// if (number % 2 === 0){
+//     console.log("четное")
+// } else {
+//     console.log("нечетное")
+// }
+
+//TASK5
+// let a = 10
+// let b = 2
+// let result = a / b
+// if (result % 2 === 0){
+//     console.log("четное")
+// } else {
+//      console.log("нечетное")
+// }
+
+//TASK6
+// let s = 5
+// if ( typeof s === `boolean` ){
+//     console.log('boolean')
+// } else if ( typeof s === `number` ){
+//     console.log('number')
+// } else if ( typeof s === `string` ){
+//     console.log('string')
+// } else {
+//     console.log('error')
+// }
+
+//TASK7
+// let a = 8
+// if ( a )
+
+//TASK8
